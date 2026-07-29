52cc00e5077df405d5c6d59ee9051cfd787565bb
abcdcode <kj022220@gmail.com>
2026-07-29 09:49:29 +0900

    플레이어 이동 모션 및 코드 작성

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var movePos = new Vector2();
+        if(Input.GetKey(KeyCode.A))
+        {
+            movePos += new Vector2(-1,0);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            movePos += new Vector2(1,0);
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            movePos += new Vector2(0,-1);
+        }
+        if(Input.GetKey(KeyCode.W))
+        {
+            movePos += new Vector2(0,1);
+        }
+        if(movePos.x != 0 || movePos.y != 0)
+        {
+            animator.SetFloat("Move",1);
+        }
+        else
+        {
+            animator.SetFloat("Move",0);
+        }
+        if(movePos.x != 0)
+        {
+            this.transform.localScale = new Vector3(movePos.x == -1 ? -1 : 1,1);
+        }
+        this.transform.Translate(movePos.normalized * Speed * Time.deltaTime);
+    }
+    public float Speed => DefaultSpeed;
+    public Animator animator;
+    public const float DefaultSpeed = 2;
+}
